99c483afbe8eb71031a971a6b48cdd623add1659
Lambda Pixel <lambda.x3cvy@simplelogin.com>
2022-08-30 20:51:10 +0200

    Bresenham

--- main.cpp
+++ main.cpp
@@ -5,7 +5,88 @@
 
 #include "bitmap.h"
 
-void trace_line(
+
+void trace_line_bresenham(
+    BMP_Color framebuffer[],
+    uint32_t width, uint32_t height,
+    BMP_Color line_color,
+    int x_start, int y_start,
+    int x_end, int y_end)
+{
+    int dx = x_end - x_start;
+    int dy = y_end - y_start;
+
+    if (std::abs(dx) > std::abs(dy)) {
+        if (x_start > x_end) {
+            std::swap(x_start, x_end);
+            std::swap(y_start, y_end);
+            dx = -dx;
+            dy = -dy;
+        }
+
+        int y_inc = 1;
+
+        if (dy < 0) {
+            y_inc = -1;
+            dy = -dy;
+        }
+
+        int y = y_start;
+        int D = 2 * dy - dx;
+
+        const int D_inc_1 = 2 * dy;
+        const int D_inc_2 = 2 * (dy - dx);
+
+        for (int x = x_start; x <= x_end; x++) {
+            if (x >= 0 && x < width && y >= 0 && y < height) {
+                framebuffer[y * width + x] = line_color;
+            }
+
+            if (D < 0) {
+                D += D_inc_1;
+            } else {
+                D += D_inc_2;
+                y += y_inc;
+            }
+        }
+    } else {
+        if (y_start > y_end) {
+            std::swap(x_start, x_end);
+            std::swap(y_start, y_end);
+            dx = -dx;
+            dy = -dy;
+        }
+
+        int x_inc = 1;
+
+        if (dx < 0) {
+            x_inc = -1;
+            dx = -dx;
+        }
+
+        int x = x_start;
+        int D = 2 * dx - dy;
+
+        const int D_inc_1 = 2 * dx;
+        const int D_inc_2 = 2 * (dx - dy);
+
+        for (int y = y_start; y <= y_end; y++) {
+            if (x >= 0 && x < width && y >= 0 && y < height) {
+                framebuffer[y * width + x] = line_color;
+            }
+
+            if (D < 0) {
+                D += D_inc_1;
+            } else {
+                D += D_inc_2;
+                x += x_inc;
+            }
+        }
+    }
+}
+
+
+void trace_line_dda(
     BMP_Color framebuffer[],
     uint32_t width, uint32_t height,
     BMP_Color line_color,
@@ -64,17 +145,18 @@ int main(int argc, char* argv[])
     BMP_Color *framebuffer = new BMP_Color[width * height];
     std::memset(framebuffer, 0, sizeof(BMP_Color) * width * height);
 
-    // Specification de la couleur de la ligne
-    BMP_Color green = {
-        .b = 0,
-        .g = 255,
-        .r = 0
-    };
-
     for (int x = 0; x < width; x += 30) {
-        trace_line(
+        const float u = (float)x / float(width - 1);
+
+        BMP_Color line_color = {
+            .b = (uint8_t)(u * 255.f),
+            .g = 255,
+            .r = (uint8_t)((1.f - u) * 255.f)
+        };
+
+        trace_line_bresenham(
             framebuffer, width, height, 
-            green, 
+            line_color, 
             x, 0,
             width/2, height/2);
     }
